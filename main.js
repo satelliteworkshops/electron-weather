85f65c0e8ca46abae7b2534283fb22506cc54e00
Eric Hollenberry <hollenberry@github.com>
2017-05-21 12:52:39 +0100

    Apply working steps

--- main.js
+++ main.js
@@ -8,81 +8,10 @@ let window = undefined
 
 process.env.GOOGLE_API_KEY = "AIzaSyAjQz5Scgjt8A-n_PKQWjNiYg-ht2gUJts"
 
+var menubar = require('menubar')
+var mb = menubar()
 
-// Don't show the app in the dock
-app.dock.hide()
-
-app.on('ready', () => {
-  createTray()
-  createWindow()
+mb.on('ready', function ready () {
+  console.log('app is ready')
+  // your app code here
 })
-
-// Quit the app when the window is closed
-app.on('window-all-closed', () => {
-  app.quit()
-})
-
-const createTray = () => {
-  tray = new Tray(path.join(assetsDirectory, 'sunTemplate.png'))
-  tray.on('right-click', toggleWindow)
-  tray.on('double-click', toggleWindow)
-  tray.on('click', function (event) {
-    toggleWindow()
-
-    // Show devtools when command clicked
-    if (window.isVisible() && process.defaultApp && event.metaKey) {
-      window.openDevTools({mode: 'detach'})
-    }
-  })
-}
-
-const getWindowPosition = () => {
-  const windowBounds = window.getBounds()
-  const trayBounds = tray.getBounds()
-
-  // Center window horizontally below the tray icon
-  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
-
-  // Position window 4 pixels vertically below the tray icon
-  const y = Math.round(trayBounds.y + trayBounds.height + 4)
-
-  return {x: x, y: y}
-}
-
-const createWindow = () => {
-  window = new BrowserWindow({
-    show: false,
-    frame: false,
-    fullscreenable: false,
-    resizable: false,
-    transparent: true,
-    webPreferences: {
-      // Prevents renderer process code from not running when window is
-      // hidden
-      backgroundThrottling: false
-    }
-  })
-  window.loadURL(`file://${path.join(__dirname, 'index.html')}`)
-
-  // Hide the window when it loses focus
-  window.on('blur', () => {
-    if (!window.webContents.isDevToolsOpened()) {
-      window.hide()
-    }
-  })
-}
-
-const toggleWindow = () => {
-  if (window.isVisible()) {
-    window.hide()
-  } else {
-    showWindow()
-  }
-}
-
-const showWindow = () => {
-  const position = getWindowPosition()
-  window.setPosition(position.x, position.y, false)
-  window.show()
-  window.focus()
-}
